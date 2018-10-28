deed434deef1be860ce5f8ba2dbbf1773b67ed81
Jonater <james.nguyen@stu.scott.kyschools.us>
2018-10-28 16:24:03 -0400

    Add files via upload
    Powerup prefab and script

---
+++ PowerUpScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour {
+/*Make sure that both the player and the powerup have colliders on. Also, make sure that either collider has "Is Trigger" enabled.
+(Preferably the powerup). Make sure that the powerup has the correct tag.
+*/
+void OnTriggerEnter2D(Collider2D collision){
+	if(collision.tag == "Player"){
+		//Do Something
+		Destroy(this.gameObject);
+		}
+    
+	}
+}
